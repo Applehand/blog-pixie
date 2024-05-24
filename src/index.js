18a401307cdd5e0c3df287952bb411f2e50c2449
Applehand <applehand@gmail.com>
2024-05-24 09:38:50 -0500

    removed unecessary function

--- src/index.js
+++ src/index.js
@@ -10,23 +10,3 @@ root.render(
   </React.StrictMode>
 );
 
-
-const initializeCropper = () => {
-  const image = document.getElementById('image');
-  if (image) {
-    const Cropper = window.Cropper;
-    const cropper = new Cropper(image, {
-      aspectRatio: 16 / 9,
-      crop(event) {
-        console.log(event.detail.x);
-        console.log(event.detail.y);
-        console.log(event.detail.width);
-        console.log(event.detail.height);
-        console.log(event.detail.rotate);
-        console.log(event.detail.scaleX);
-        console.log(event.detail.scaleY);
-      },
-    });
-  }
-};
-
